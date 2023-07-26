fea4e0e83b5a4727ffb929901f78815ea2b8b60b
fog <38463346+fog-zs@users.noreply.github.com>
2023-07-26 10:10:24 +0900

    modified:   gatsby-config.ts modified:   package.json

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,9 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https:/fog-zs.github.io/speech-to-text/`,
   },
+  pathPrefix: `/speech-to-text`,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
